e3a40ff135aac24d0d1f33289153adc9aa39bccb
tomash <tomash>
2001-12-07 12:36:02 +0000

    Test receiver for streaming

---
+++ teststream.cpp
@@ -0,0 +1,21 @@
+#include <cc++/socket.h>
+#include <cc++/thread.h>
+#include "alpp.h"
+
+using namespace openalpp;
+
+int main() {
+  try {
+    UDPSocket socket(InetAddress("127.0.0.1"),33333);
+    NetStream stream(&socket,Mono8,11025,1024);
+    Source source(stream);
+    source.SetAmbient();
+    source.Play();
+
+    ccxx_sleep(10000);
+  } catch(Error e) {
+    cerr << e << "\n";
+  }
+
+  return 0;
+}
