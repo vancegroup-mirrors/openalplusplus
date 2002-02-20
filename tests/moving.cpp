2dc753d7c7f8fcd2e9eecc9b0593d8136d98503f
andersb <andersb>
2002-02-20 13:35:58 +0000

    no message

--- tests/moving.cpp
+++ tests/moving.cpp
@@ -1,4 +1,27 @@
 /**
+ * OpenAL++ - an object oriented toolkit for spatial sound
+ * Copyright (C) 2002 VRlab, Umeå University
+ *
+ * OpenAL++ was created using the libraries:
+ *                 OpenAL (http://www.openal.org), 
+ *              PortAudio (http://www.portaudio.com/), and
+ *              CommonC++ (http://cplusplus.sourceforge.net/)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ *
+ *  
  * Simple example for using OpenAL++.
  * Plays a file bee.wav and moves it left right. 
  */
@@ -29,16 +52,19 @@ int main() {
     beesound.SetPosition(limits[0],0.0,0.0);
 	  beesound.Play();
 
+    const int no_laps=5;
+
+    std::cerr << "Moving sound 5 times..." << std::endl;
 
     // Do a cheat time loop.
-    while(angle<(M_PI*60)) {
+    while(angle<(M_PI*no_laps)) {
 	    ccxx_sleep(delay); // Wait for delay milliseconds
 
       time +=delay/1000; // Calculate the time in the loop
       angle=M_PI *time;  // What is the resulting angle
       
       // Calculate a new position
-      beesound.SetPosition(1.0*cos(angle),0.0,1.0*sin(angle));
+      beesound.SetPosition(limits[0]*cos(angle),0.0,limits[1]*sin(angle));
 	  }
   } catch(Error e) {
     cerr << e << "\n";
